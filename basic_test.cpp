68ca1be79d452f51d3377c117a3fffbdaa49cd6a
henryjykim <henry.jykim@gmail.com>
2013-12-10 16:47:44 +0900

    basic test

--- basic_test.cpp
+++ basic_test.cpp
@@ -1,13 +1,39 @@
 #include <cassert>
+#include <iostream>
+#include <thread>
 #include <rocksdb/db.h>
 #include <rocksdb/statistics.h>
 
+using namespace std;
+using namespace rocksdb;
+
+void put_test(DB* db, Options options) {
+    const string value = "value";
+    for (int i = 0; i < 100; i++) {
+        db->Put(WriteOptions(), to_string(i), to_string(i*i));
+    }
+}
+
+void print_result(DB* db) {
+    const string key = "key";
+    string value;
+    for (int i = 0; i < 100; i++) {
+        Status status = db->Get(ReadOptions(), to_string(i), &value);
+        cout << value << endl;
+    }
+}
+
 int main() {
-    rocksdb::DB* db;
-    rocksdb::Options options;
+    int thread_count = 1;
+    DB* db;
+    Options options;
     options.create_if_missing = true;
-    options.statistics = rocksdb::CreateDBStatistics();
-    rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/rocks.db", &db);
-    assert(status.ok());
+    options.statistics = CreateDBStatistics();
+    Status status = DB::Open(options, "/tmp/rocks.db", &db);
+    thread t1(put_test, db, options);
+    t1.join();
+
+    print_result(db);
+
     return 0;
 }
